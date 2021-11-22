f1974c25ef0ebe998af6cc20f77f0bc5da6cf9aa
Pavel Kaznachei <pavel.kaznachei@gmail.com>
2021-11-22 12:53:29 +0200

    Add provider

---
+++ Module2HW4/Providers/PlantProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Module2HW4.Providers.Abstractions;
+using Module2HW4.Models.Shrubs;
+using Module2HW4.Models.Trees;
+
+namespace Module2HW4.Models.Providers
+{
+    public class PlantProvider : IPlantProvider
+    {
+        public Plant[] Get()
+        {
+            return new Plant[]
+                {
+                    new Gooseberry(),
+                    new Parsley(),
+                    new Raspberry(),
+                    new Valerian(),
+                    new Cedar() { GrowthRingCount = 30 },
+                    new Cedar() { GrowthRingCount = 50 },
+                    new Maple() { GrowthRingCount = 25 },
+                    new Oak() { GrowthRingCount = 40 },
+                    new Pine() { GrowthRingCount = 50 }
+                };
+        }
+    }
+}
